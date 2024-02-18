dbb4098d0ad3657ff7ea61befcb1e893e9c44106
Hossein Rezaei Fard <57575843+Hosseinrezaeifard@users.noreply.github.com>
2024-02-18 20:26:05 +0330

    Update calculator.sol

--- calculator.sol
+++ calculator.sol
@@ -1,1 +1,25 @@
 
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+contract Calculator {
+    uint256 result = 0;
+
+    function add(uint256 num) public {
+        result += num;
+    }
+
+    function subtract(uint256 num) public {
+        result -= num;
+    }
+
+    function multiply(uint256 num) public {
+        result *= num;
+    }
+
+    // view means we're not changing the data
+    function get() public view returns (uint256) {
+        return result;
+    }
+}
